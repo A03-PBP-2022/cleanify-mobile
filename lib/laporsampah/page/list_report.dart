16e05a6730693fce53c0181f8ae7ad41eaa5b5df
Hans TM <112265332+HansTM@users.noreply.github.com>
2022-12-02 23:36:59 +0700

    Lapor: Hapus dart:html lagi

--- lib/laporsampah/page/list_report.dart
+++ lib/laporsampah/page/list_report.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 import 'dart:convert';
-import 'dart:html';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:cleanify/core/drawer.dart';
