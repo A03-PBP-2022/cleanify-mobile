ca8a47a89bb4d5ca83f23bbe18d40884d3fb93ce
thaliafortuna <thaliafortuna8@gmail.com>
2022-12-02 20:03:14 +0700

    drawer fixes

--- lib/core/drawer.dart
+++ lib/core/drawer.dart
@@ -34,6 +34,26 @@ class GlobalDrawer extends StatelessWidget {
               );
             },
           ),
+                    ListTile(
+            title: const Text('List Bank Sampah'),
+            onTap: () {
+              // Route menu ke halaman utama
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyBankSampahPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Form Bank Sampah'),
+            onTap: () {
+              // Route menu ke halaman utama
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyFormPage()),
+              );
+            },
+          ),
         ],
       ),
     );
