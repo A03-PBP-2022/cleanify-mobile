ff5623d7e287811a536e832bc52a64b4bd7ea337
naiyayunir <naiya.dwita@ui.ac.id>
2022-12-05 19:12:55 +0700

    update login register

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:cleanify/authentication/page/loginPage.dart';
 import 'package:cleanify/core/home.dart';
 import 'package:flutter/material.dart';
 import 'package:cleanify/blog/page/index.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -48,3 +51,49 @@ class MyApp extends StatelessWidget {
         );
     }
 }
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    // drawer: NavigationDrawerWidget(),
+    // // endDrawer: NavigationDrawerWidget(),
+    // appBar: AppBar(
+    //   title: Text(MyApp.title),
+    // ),
+    body: Builder(
+      builder: (context) => Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        
+        //Coba ngeget
+        child: ElevatedButton(
+          onPressed: () async {
+            final response = await http.get(
+              Uri.parse("https://cleanifyid.up.railway.app/auth/login/"),
+                headers: <String, String>{
+                  'Content-Type': 'application/json;'
+                },
+            );
+            print(response.body);
+            Map<String, dynamic> map = jsonDecode(response.body);
+          },
+          style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(16.0), backgroundColor: Colors.green,
+          ),
+          child: Text(
+              "TESTTT get",
+              style: TextStyle(color: Colors.white),
+            ),
+        ),
+        
+      ),
+    ),
+  );
+}
+
+
